1a82ebe045a6e288155eb2ba85a56d54e282f152
StefanoM99 <stefano.mazziottaa@gmail.com>
2022-05-10 22:06:34 +0200

    impl server

--- ws/server.js
+++ ws/server.js
@@ -1,21 +1,19 @@
-const WebSocket = require('ws');
+const express = require("express");
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors:{origin : "*"} });
 
-const wss = new WebSocket.Server({ port: 1337 });
+//
 
-// var active_connection = null;
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    ws.send('something');
-  });
-  //active_connection = ws;
-  ws.send('something');
-  //test();
+server.listen(3001, () =>{
+  console.log("Server running ...")
 });
 
-/*
-function test(){
-  active_connection.send('something else');
-}
-*/
+io.on('connection', (socket) => {
+  socket.on('Message', (msg) => {
+    console.log('message: ' + msg);
+  
+  });
+});
+
+
