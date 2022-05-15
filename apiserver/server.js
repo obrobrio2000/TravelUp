860c1d9ee94ab6ccc1ecfdf15b10cac406403780
Filippo Serafini <filipposerafini2000@gmail.com>
2022-05-15 02:19:02 +0200

    Aggiunto link per la dashboard nella creazione itinerario, modificato il docker compose per far partire il server api, aggiunti file base serverapi

--- apiserver/server.js
+++ apiserver/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const request = require('request');
 
 const port = 1717;
 
 const app = express();
+
+var opentrip = "https://api.opentripmap.com/0.1";
+
+function getCityInfo(city,distance){
+
+
+
+}
+
+
 
 app.listen(port, () => {
     console.log(`Server in ascolto sull'indirizzo http://localhost:${port}`);
